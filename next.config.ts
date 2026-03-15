2bcaccfb1bc6bc2b33488f25734d7b971b427f75
yahia008 <yahyatijjani99@gmail.com>
2026-03-15 12:52:12 +0100

    toml

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-  output: "export",      // enable static HTML export
-  trailingSlash: true, 
+  /* config options here */
 };
 
 export default nextConfig;
